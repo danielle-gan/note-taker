446399112fa8a0d8664b00d5c9f0ec29888982d0
danielle-gan <danielle.gan@uconn.edu>
2022-02-13 11:37:52 -0500

    still tinkering with the routes

--- server.js
+++ server.js
@@ -11,7 +11,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-// API ROUTES
+//ROUTES
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+})
+
 app.get('/api/notes', (req, res) => {
     let response = notes;
     res.json(response);
@@ -27,10 +31,6 @@ app.post('/api/notes', (req, res) => {
 // HTML ROUTES
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
-})
-
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/notes.html'));
 })
 
 app.listen(PORT, () => {
